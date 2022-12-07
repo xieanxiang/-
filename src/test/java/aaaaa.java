f721b611c19317befc85713b53f962a9423ceeea
xieanxiang <2427873503@qq.com>
2022-12-07 19:04:22 +0800

    新功能

--- src/test/java/aaaaa.java
+++ src/test/java/aaaaa.java
@@ -1,5 +1,5 @@
 public class aaaaa {
     public static void main(String[] args) {
-        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa2222222222222222222222222222222222222222222222222222222");
     }
 }
